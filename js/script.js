68d5efc125092e0764a667ac23a06e508a498587
agistawarda <agistawarda.com>
2026-09-13 00:43:05 +0700

    penambahan section jam buka dan lokasi

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
-  promoButton.textContent = "Promo: Beli 2 gratis tester!";
-  console.log("Promo Kopi Nusa berhasil ditampilkan.");
+  promoButton.textContent = "Beli 2 Kopi, Gratis 1!";
+  console.log("Promo Ruang Kopi berhasil ditampilkan.");
 });
